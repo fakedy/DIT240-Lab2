7c3dbc78894688070ef071dd932ce620dabeec38
fakedy <34602297+fakedy@users.noreply.github.com>
2025-11-20 14:51:46 +0100

    yolo

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,33 +1,50 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
-
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+)
 
 type Coordinator struct {
 	// Your definitions here.
+}
 
+var ourFiles []File
+
+type File struct {
+	filename string
+	state    int
 }
 
 // Your code here -- RPC handlers for the worker to call.
 
-//
 // an example RPC handler.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
 
+func (c *Coordinator) AssignTask(args *Arguments, reply *Reply) error {
 
-//
+	// loop through our files and assign a task that have not been processed yet
+	for i := range ourFiles {
+		fmt.Printf("filename: %s\nstate: %d\n", ourFiles[i].filename, ourFiles[i].state)
+		if ourFiles[i].state == 0 {
+			reply.Filename = ourFiles[i].filename
+			ourFiles[i].state = 1 // set state to "in progress"
+			return nil
+		}
+	}
+	return nil
+}
+
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -41,29 +58,32 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
-//
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
 	ret := false
 
 	// Your code here.
 
-
 	return ret
 }
 
-//
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
-//
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	c := Coordinator{}
 
 	// Your code here.
 
+	// fill an array of File structs
+	for i := range files {
+		ourFiles = append(ourFiles, File{filename: files[i], state: 0})
+	}
+
+	for i := range ourFiles {
+		fmt.Println(ourFiles[i].filename)
+	}
 
 	c.server()
 	return &c
--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -6,8 +6,10 @@ package mr
 // remember to capitalize all names.
 //
 
-import "os"
-import "strconv"
+import (
+	"os"
+	"strconv"
+)
 
 //
 // example to show how to declare the arguments
@@ -24,6 +26,13 @@ type ExampleReply struct {
 
 // Add your RPC definitions here.
 
+type Arguments struct {
+	X int
+}
+
+type Reply struct {
+	Filename string
+}
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
